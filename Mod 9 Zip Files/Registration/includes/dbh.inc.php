7e4b722ed8384b3024e27e0dd46ffbfff872015b
redbaron131 <andrew.danielson@hotmail.com>
2023-11-26 20:13:59 -0500

    Mod 9 zip files

---
+++ Mod 9 Zip Files/Registration/includes/dbh.inc.php
@@ -0,0 +1,11 @@
+<?php
+$dsn = "mysql:host=localhost;dbname=MoffitBayLodge";
+$dbusername = "root";
+$dbpassword = "";
+
+try {
+    $pdo = new PDO($dsn, $dbusername, $dbpassword);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "connection failed: " . $e->getMessage();
+}
